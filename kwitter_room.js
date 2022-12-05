30d006900a106b32d3ae70867fb35ce70f130dec
InFlamIty <107593393+InFlamIty@users.noreply.github.com>
2022-12-04 19:34:03 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC-pExsidvSszHtMOY-5Z8qL-vzkOP7cxM",
+    authDomain: "my-kwitter-79727.firebaseapp.com",
+    databaseURL: "https://my-kwitter-79727-default-rtdb.firebaseio.com",
+    projectId: "my-kwitter-79727",
+    storageBucket: "my-kwitter-79727.appspot.com",
+    messagingSenderId: "695315657970",
+    appId: "1:695315657970:web:5944468fe10f25738d0c6b"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
